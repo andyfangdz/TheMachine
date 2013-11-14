b506ec3269c97f6000f592386a958736f8a75315
Andy Fang <yzmtf2008@gmail.com>
2013-11-15 00:52:33 +0800

    First commit

---
+++ machine_define.py
@@ -0,0 +1,23 @@
+level_refrence={
+  0:"Irrelevant",
+  1:"Relevant",
+  2:"Critical",
+  3:"Machine"
+}
+refrence_level={v:k for k, v in level_refrence.items()}
+#code from http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
